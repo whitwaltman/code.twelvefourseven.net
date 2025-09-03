914e8d55020ebaaf15592e62dd6f0eae687b5716
Whit Waltman <whitwaltman@mailbox.org>
2025-09-02 19:28:34 -0700

    add dynamic routing for snippets

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,11 +2,12 @@
 import { defineCollection, z } from 'astro:content';
 
 // import loader(s)
-import { glob, file } from 'astro/loaders';
+// import { glob, file } from 'astro/loaders';
+import { glob } from 'astro/loaders';
 
 // define collections
-const notes = defineCollection({
-    loader: glob({ pattern: "**/*.md", base: "./src/notes" }),
+const notesCollection = defineCollection({
+    loader: glob({ pattern: "**/[^_]*.md", base: "./src/content/notes" }),
     schema: z.object({
         title: z.string(),
         created: z.coerce.date(),
@@ -19,12 +20,15 @@ const notes = defineCollection({
 //     loader: file("src/data/glossary.json"),
 // });
 
-const snippets = defineCollection({
-    loader: glob({ pattern: "**/*.md", base: "./src/snippets" }),
+const snippetsCollection = defineCollection({
+    loader: glob({ pattern: "**/*.md", base: "./src/content/snippets" }),
     schema: z.object({
         title: z.string(),
         tags: z.array(z.string()),
     }),
 });
 
-export const collections = { notes, snippets };
+export const collections = {
+    notes: notesCollection, 
+    snippets: snippetsCollection,
+};
