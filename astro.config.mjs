3ee34becb252c16b4e03d9feed045b0c83a5edca
Whit Waltman <whitwaltman@mailbox.org>
2025-09-25 10:45:57 -0700

    reconfigure as 11ty project

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({
-    markdown: {
-        syntaxHighlight: false,     // disable built-in highlighting w/ Prism
-    }
-});
--- src/content/notes/hello-world.md
+++
@@ -1,6 +0,0 @@
----
-title: "Hello, world!"
-created: 2025-09-02
-tags: [meta]
----
-This is just a placeholder note while I define the collection schemas in my content config.
