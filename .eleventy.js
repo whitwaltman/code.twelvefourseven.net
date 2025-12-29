b7888c09a8e9462d64ed349f2a64a5d5c9ab91a5
Whit Waltman <whitwaltman@mailbox.org>
2025-12-28 18:08:20 -0800

    stash progress

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,25 @@
 export default async function (config) {
-    config.addGlobalData('layout', 'layouts/base.njk');
+    config.addGlobalData('layout', 'base.njk');
+
+    config.addCollection('notes', function (collection) {
+        return collection.getFilteredByGlob('./src/notes/*.md');
+    });
+
+    config.addCollection('posts', function (collection) {
+        return collection.getFilteredByGlob('./src/posts/*.md');
+    });
+
+    config.addCollection('tagList', function (collection) {
+        let tagSet = new Set();
+        collection.getAll().forEach((item) => {
+            ;(item.data.tags || []).forEach((tag) => tagSet.add(tag));
+        });
+        return [...tagSet];
+    })
     
-    config.addShortcode("a", function (url, text) {
+    config.addShortcode('a', function (url, text) {
         const metadata = 'class="ext-link" target="_blank" rel="noopener noreferrer"';
-        return `<a ${metadata} href="${url}">${text}<span>&nearrow;</span></a>`;
+        return `<a ${metadata} href='${url}'>${text}<span>&nearrow;</span></a>`;
     });
 
     config.setServerOptions({
@@ -15,8 +31,10 @@ export default async function (config) {
         dataTemplateEngine: 'njk',
         htmlTemplateEngine: 'njk',
         dir: {
-            input: "src",
-            output: "_site",
+            input: '.',
+            includes: 'src/_includes',
+            data: 'src/_data',
+            output: '_site',
         }
     };
 }
--- src/commands/ssh-add.md
+++
@@ -1,8 +0,0 @@
-using two or more github accounts on the same machine and having ssh credentials not match
-
-```zsh
-git rv # alias for git remote -v
-ssh-add ~/.ssh/whitwaltman
-```
-
-i just name my file the name of my github username
