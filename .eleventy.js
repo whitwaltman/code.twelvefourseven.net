81b11649eff6bc167fbb95d9da1bb2655e1792bd
Whit Waltman <whitwaltman@mailbox.org>
2026-02-25 18:44:03 -0800

    transition back to 11ty

---
+++ .eleventy.js
@@ -0,0 +1,43 @@
+import filters from "./utils/filters.js";
+import transformExternalLinks from "./utils/links.js";
+
+export default async function (config) {
+	// Add global data variable to signal build modality
+	const isProduction = process.env.NODE_ENV === "production";
+	config.addGlobalData("isProduction", isProduction);
+
+	// Copy public assets to output folder
+	config.addPassthroughCopy({
+		"./public": "/",
+	});
+
+	// Add global default layout
+	config.addGlobalData("layout", "base.njk");
+
+	// Register filter functions
+	Object.keys(filters).forEach((filter) => {
+		config.addFilter(filter, filters[filter]);
+	});
+
+	// Register imported links function as a transform
+	config.addTransform("externalLinks", transformExternalLinks);
+
+	return {
+        // control which files 11ty will process
+        templateFormats: ["md", "njk", "html"],
+
+        // pre-process *.md files with:
+		markdownTemplateEngine: "njk",
+
+        // pre-process *.html files with:
+        htmlTemplateEngine: "njk",
+
+        // directory config
+		dir: {
+			input: ".",
+			includes: "_includes",
+			data: "_data",
+			output: "_site",
+		}
+	};
+}
