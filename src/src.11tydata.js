fca0a3faa2bd8b59ee448884561f7a5ceb515335
Whit Waltman <whitwaltman@mailbox.org>
2026-03-28 11:50:18 -0700

    shit i fucked up

--- src/src.11tydata.js
+++ src/src.11tydata.js
@@ -1,17 +1,31 @@
 const isProduction = process.env.NODE_ENV === "production";
 
+const clean = (str) => {
+    if (str.length <= 30) return str;
+    const words = str.toLowerCase().split(" ");
+    let result = "";
+    let idx = 0;
+    while (result.length < 30) {
+        result += words[idx] + " ";
+        idx += 1;
+    }
+    return result.slice(0, -1) + "...";
+};
+
 const data = {
     layout: "page.njk",
     eleventyComputed: {
         permalink: (data) => (isProduction && data.draft) ? false : data.permalink,
-        
+
         eleventyExcludeFromCollections: (data) => (isProduction && data.draft) ? true : data.eleventyExcludeFromCollections,
 
         breadcrumbs: (data) => {
             if (isProduction && data.draft) return false;
             const urlPath = data.page.url.split("/").filter(Boolean);
             return urlPath.map((part, index) => {
-                let info = data.sections[part] || { label: part.replace(/-/g, " ") };
+                let info = data.sections[part] ||
+                    { label: clean(data.title) } ||
+                    { label: part.replace(/-/g, " ") };
 
                 if (index === 1 && urlPath[0] === "snippets" && data.sections.snippets.children?.[part]) {
                     info = data.sections.snippets.children[part];
