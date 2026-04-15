c575b6ae6cfe9418902225773941c627756b55bf
Whit Waltman <whitwaltman@mailbox.org>
2026-04-14 18:58:25 -0700

    migrating content

--- _utils/filters.js
+++ _utils/filters.js
@@ -30,14 +30,6 @@ const filters = {
 		}
 		return result.slice(0, -1) + "...";
 	},
-
-	filterBySnippetCategory: function (collection, category) {
-		return collection.filter((item) => {
-			const parts = item.filePathStem.split("/");
-			const itemCategory = (parts[1] === "snippets" && parts.length > 3) ? parts[2] : null;
-			return itemCategory === category;
-		});
-	},
 };
 
 export default filters;
--- src/ref/tmux/sessions.md
+++
@@ -1,8 +0,0 @@
----
-title: tmux sessions
-tags: ["scrap"]
-ideas: ["variations", "adaptation", "snake"]
-platforms: ["browser", "mobile-friendly"]
-tools: ["phaser"]
----
-Not sure yet how I'm gonna organize these bits. Might even use some data files or something.
